23b7d864db20cbd9063a961256a7954ecf3f3cfd
Anany Agrawal <72305931+ananyagrawal@users.noreply.github.com>
2022-10-09 11:45:55 +0530

    Update bmi.java

--- bmi.java
+++ bmi.java
@@ -1,6 +1,5 @@
 public class bmi {
     public static void main(String a1[]){
-        System.out.println("Name: Anany Agrawal SAP ID: 1000014386");
         double w,h,bmi;
         w=Double.parseDouble(a1[0]);
         h=Double.parseDouble(a1[1]);
