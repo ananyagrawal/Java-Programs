cc0c732caf9f826d7968d3986ca166adcec2b245
Anany Agrawal <ananyagrawal698@gmail.com>
2022-10-14 20:40:41 +0530

    I wanna be the guy

---
+++ CODEFORCES/iwannaBeTheGuy.java
@@ -0,0 +1,9 @@
+package CODEFORCES;
+import java.util.*;
+public class iwannaBeTheGuy {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+    }
+}
