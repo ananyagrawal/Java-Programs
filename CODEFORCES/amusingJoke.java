e8555f40531d685491d2c4c101a78c834cfca2ef
Anany Agrawal <ananyagrawal698@gmail.com>
2022-11-09 11:29:49 +0530

    improving

--- CODEFORCES/amusingJoke.java
+++ CODEFORCES/amusingJoke.java
@@ -11,19 +11,10 @@ public class amusingJoke {
         char b[]=pile.toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
-        if(a.length==b.length){
-            int flag=0;
-            for(int i=0;i<a.length;i++){
-                if(a[i]!=b[i]){
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0)
-            System.out.println("YES");
-            else
-            System.out.println("NO");
-        }
+        String x= new String(a);
+        String y= new String(b);
+        if(x.equals(y))
+        System.out.println("YES");
         else
         System.out.println("NO");
         sc.close();
