38643e5192f70263500c23d48420fe21b7df590e
Anany Agrawal <ananyagrawal698@gmail.com>
2023-01-16 20:09:00 +0530

    Union of two arrays

---
+++ GFG/unionTwoArrays.java
@@ -0,0 +1,17 @@
+package GFG;
+
+import java.util.*;
+
+public class unionTwoArrays {
+
+    public static int doUnion(int a[], int n, int b[], int m) {
+        // Your code here
+        Set<Integer> s = new HashSet<Integer>();
+        for (int i = 0; i < n; i++)
+            s.add(a[i]);
+        for (int i = 0; i < m; i++)
+            s.add(b[i]);
+        return s.size();
+    }
+
+}
