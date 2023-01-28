32d4a350032c5621a3505a895464296c3dca6b61
Anany Agrawal <ananyagrawal698@gmail.com>
2023-01-28 21:14:28 +0530

    technical training assignment

--- reverse.java
+++ reverse.java
@@ -1,8 +1,17 @@
 import java.util.*;
+
 public class reverse {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        int res = 0;
+        while (n > 0) {
+            res = res + n % 10;
+            n = n / 10;
+            res = res * 10;
+        }
+        System.out.println(res / 10);
         sc.close();
     }
 }
